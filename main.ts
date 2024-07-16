999399b975d4b31bc9998a235943971aeecb8df4
Ishwa Younus <160063919+iym-555@users.noreply.github.com>
2024-07-17 03:04:14 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,92 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber : number = Math.floor(10000 + Math.random() * 90000);
+
+let myBalance : number = 0 
+
+let answer = await inquirer.prompt(
+    [
+        {
+            name: "students",
+            type: "input",
+            message:"Enter student name:",
+            validate: function(value){
+                if (value.trim() !== ""){
+                    return true;
+                }
+                return "Please enter a non-empty value.";
+            },
+        },
+        {
+            name: "courses",
+            type: "list",
+            message: "Select the course to enroll in",
+            choices: ["TypeScript", "Javascript", "MSOffice", "Python", "HTML"]
+        }
+    ]
+);
+const tutionFee: {[key:string]: number} = {
+    "TypeScript": 6000,
+    "Javascript": 5000,
+    "MSOffice": 2000,
+    "Python": 10000,
+    "HTML": 2500    
+};
+
+console.log(`\nTution Fees: ${tutionFee[answer.courses]}/-\n`);
+console.log(`Balance: ${myBalance}\n`); 
+
+let paymentType = await inquirer.prompt(
+    [
+        {
+            name: "payment",
+            type: "list",
+            message: "Select payment method",
+            choices: ["Bank Transfer", "Easypaisa", "Jazzcash"]
+        },
+        {
+            name: "amount",
+            type: "input",
+            message: "Transfer Money:",
+            validate: function (value) {
+                if (value.trim() !== "") {
+                    return true;
+                }
+                return "Please enter a non-empty value.";
+            },
+        }
+    ]
+);
+console.log(`\nYou select payment method ${paymentType.payment}\n`);
+
+const tutionFees = tutionFee[answer.courses];
+const paymentAmount = parseFloat(paymentType.amount)
+
+if(tutionFees == paymentAmount){
+    console.log(`Congratulations, You have sucessfully enrolled in ${answer.courses}.\n`); 
+
+    let ans= await inquirer.prompt(
+        [
+            {
+                name: "select",
+                type: "list",
+                message: "What would you like to do next?",
+                choices: ["View status", "Exit"]
+            }
+        ]
+    )
+    if (ans.select === "View status"){
+        console.log("\n***************STATUS******************\n");
+        console.log(`Student Name: ${answer.students}`);
+        console.log(`Student ID: ${randomNumber}`);
+        console.log(`Course: ${answer.courses}`);
+        console.log(`Tution Fees Paid: ${paymentAmount}`);
+        console.log(`Balance: ${myBalance += paymentAmount}`);       
+    }else {
+        console.log("\nExiting Student Management System");
+    }
+}else {
+    console.log("Invalid amount due to course\n");
+};
